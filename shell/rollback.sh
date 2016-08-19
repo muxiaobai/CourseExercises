82ad8dd7862f042de8f44e5ec835d8d8253cc1c3
慕小白 <zpf12345678910@gmail.com>
2016-08-19 09:30:48 +0800

    shell update

--- shell/rollback.sh
+++ shell/rollback.sh
@@ -2,29 +2,29 @@
 
 # Author : muxiaobai
 
-#chmod +x ./test.sh  #使脚本具有执行权限
-#./test.sh  #执行脚本
-#TOMCAT_HOME="/user/local/apache-tomcat-8.0.36"
-#LOG_HOME="/data/shelllog/upgrade"
-#WAR_HOME="/data/war"
-TOMCAT_HOME="/c/apache-tomcat-8.0.36"
+#chmod +x ./test.sh  
+#./test.sh  
+TOMCAT_HOME="/usr/local/apache-tomcat-8.0.36"
+LOG_HOME="/data/shelllog/upgrade"
+WAR_HOME="/data/war"
+#TOMCAT_HOME="/c/apache-tomcat-8.0.36"
 TOMCAT_PORT=8080
 FILE_NAME=`date "+%Y%m%d%H%M%S"`
-LOG_HOME="/c/Users/zhang/Desktop"
+#LOG_HOME="/c/Users/zhang/Desktop"
 LOG_FILE_NAME="$FILE_NAME.log"
 LOG_FILE_PATH="$LOG_HOME/$LOG_FILE_NAME"
 
-WAR_HOME="$LOG_HOME"
+#WAR_HOME="$LOG_HOME"
 
 TIME=`date "+%Y-%m-%d %H:%M:%S"`
 #FILE_NAME="upload"
 currentTime=`date "+%Y-%m-%d %H:%M:%S"`
-echo "脚本开始执行时间：$currentTime" >> "$LOG_FILE_PATH"
-startupsh="$TOMCAT_HOME/bin/startup.bat"
-shutdownsh="$TOMCAT_HOME/bin/shutdown.bat"
-echo "LOG文件路径：$LOG_FILE_PATH" >> "$LOG_FILE_PATH"
-echo "启动Tomcat脚本地址：$startupsh" >>"$LOG_FILE_PATH"
-echo "关闭tomcat脚本地址：$shutdownsh" >>"$LOG_FILE_PATH"
+echo "begin time :$currentTime" >> "$LOG_FILE_PATH"
+startupsh="$TOMCAT_HOME/bin/startup.sh"
+shutdownsh="$TOMCAT_HOME/bin/shutdown.sh"
+echo "LOG location:$LOG_FILE_PATH" >> "$LOG_FILE_PATH"
+echo "start Tomcat URL:$startupsh" >>"$LOG_FILE_PATH"
+echo "stop Tomcat URL:$shutdownsh" >>"$LOG_FILE_PATH"
 
 
 
@@ -32,9 +32,9 @@ echo "关闭tomcat脚本地址：$shutdownsh" >>"$LOG_FILE_PATH"
 # if tomcat is run should stop .else skip this  `stop tomcat` step 
 
 cd $TOMCAT_HOME
-	echo "$TIME 正在关闭tomcat..." >>"$LOG_FILE_PATH"
+	echo "$TIME stop Tomcat..." >>"$LOG_FILE_PATH"
 	$shutdownsh
-	echo "tomcat已经关闭" >>"$LOG_FILE_PATH"
+	echo "tomcat stoped" >>"$LOG_FILE_PATH"
 sleep 2s;
 
 # remove old war and file
@@ -51,11 +51,11 @@ newest_file_of()
 echo "newest file of *.war is $(newest_file_of *.war)"
 LAST_NAME=$(newest_file_of *.war)
 
-echo "找到最后一次的文件: $LAST_NAME" >> "$LOG_FILE_PATH"
+echo "last file: $LAST_NAME" >> "$LOG_FILE_PATH"
 cp $WAR_HOME/$LAST_NAME $TOMCAT_HOME/webapps/ROOT.war
 
 #start tomcat 
 cd $TOMCAT_HOME
-echo " $TIME 正在启动Tomcat..." >>"$LOG_FILE_PATH"
+echo " $TIME start Tomcat..." >>"$LOG_FILE_PATH"
 $startupsh
-echo "$TIME 启动Tomcat成功..." >>"$LOG_FILE_PATH"
+echo "$TIME Tomcat started..." >>"$LOG_FILE_PATH"
