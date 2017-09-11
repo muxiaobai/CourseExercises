ee6915f7747ee4e61e355d7afd40bc7e890f9eb3
muxiaobai <zpf12345678910@gmail.com>
2017-09-11 17:49:29 +0800

    :rotating_light:Sort

--- java/ProjectTest/src/org/sun/sort/InsertSort.java
+++ java/ProjectTest/src/org/sun/sort/InsertSort.java
@@ -20,7 +20,7 @@ package org.sun.sort;
  */
 public class InsertSort {
    public static void main(String[] args) {
-      //插入
+        //插入
 //       straightInsert();
        binaryInsert();
 //       ShellInsert();
@@ -47,10 +47,6 @@ public class InsertSort {
                    a[j+1] = a[j];
                }else{
                    break;
-               }
-               System.out.println("\nstep :"+i+",inner:"+j);
-               for (int k = 0; k < a.length; k++) {
-                   System.out.print(a[k]+" ");
                }
            }
            a[j+1] = temp;//最后把当前位置赋上初始待插入的值
@@ -83,6 +79,7 @@ public class InsertSort {
            int left = 0;
            int right = i-1;
            int mid = 0;
+           //找位置
            while(left<=right){
                mid = (left+right)/2;
                if(temp<a[mid]){
@@ -91,6 +88,7 @@ public class InsertSort {
                    left = mid+1;
                }
            }
+           //赋值
            for (int j = i-1; j >= left; j--) {
                a[j+1] = a[j];
            }
@@ -107,11 +105,42 @@ public class InsertSort {
    /**
     * Shell也是用的插入
     * ShellInsert:()
+    * 先取一个小于n的整数d1作为第一个增量，把文件的全部记录分成d1个组。
+    * 所有距离为d1的倍数的记录放在同一个组中。先在各组内进行直接插入排序；
+    * 然后，取第二个增量d2<d1重复上述的分组和排序，直至所取的增量dt=1(dt<dt-l<…<d2<d1)，
+    * 即所有记录放在同一组中进行直接插入排序为止。该方法实质上是一种分组插入方法。
     * @author Mu Xiaobai
     * @since JDK 1.8
     */
    public static void  ShellInsert(){
-       
+       int[] a={49,38,65,97,76,13,27,49,78,34,12,64,1};
+       System.out.println("排序之前：");
+       for (int i = 0; i < a.length; i++) {
+           System.out.print(a[i]+" ");
+       }
+       //希尔排序
+       int d = a.length;
+       while(true){
+           d = d / 2;
+           for(int x=0;x<d;x++){
+               for(int i=x+d;i<a.length;i=i+d){
+                   int temp = a[i];
+                   int j;
+                   for(j=i-d;j>=0&&a[j]>temp;j=j-d){
+                       a[j+d] = a[j];
+                   }
+                   a[j+d] = temp;
+               }
+           }
+           if(d == 1){
+               break;
+           }
+       }
+       System.out.println();
+       System.out.println("排序之后：");
+       for (int i = 0; i < a.length; i++) {
+           System.out.print(a[i]+" ");
+       }
    }
 }
 
