0d85f8d91e905a399584be764ee040f51f499e41
mu xiaobai <zpf12345678910@gmail.com>
2017-10-21 09:57:49 +0000

    mnist

--- python/tensorflow/mnist/mnist.py
+++ python/tensorflow/mnist/mnist.py
@@ -3,19 +3,19 @@
 import tensorflow as tf
 import input_data
 mnist = input_data.read_data_sets("MNIST_data/" , one_hot = True)
-batch_xs,batch_ys = mnist.train.next_batch(100)
+#batch_xs,batch_ys = mnist.train.next_batch(100)
 #  
 X = tf.placeholder(tf.float32, shape= [None , 784])
 Y = tf.placeholder(tf.float32, shape= [None , 10])
 
-W = tf.Variable(tf.zeros([784 , 10]))
-b = tf.Variable(tf.zeros([10]))
+W = tf.Variable(tf.random_normal([784 , 10]))
+b = tf.Variable(tf.random_normal([10]))
 hypothesis = tf.nn.softmax(tf.matmul(X,W) + b)
 
 # 成本函数 : 利用交叉熵来描述损失函数
 loss = tf.reduce_mean(- tf.reduce_sum(Y * tf.log(hypothesis),1))
 # 优化算法 ---梯度下降算法  gradient descent algorithm
-train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 # 初始化变量
 init = tf.initialize_all_variables()
@@ -23,11 +23,29 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 # 拟合平面
-for step in xrange(0,10001):
-    loss_val,_  = sess.run([loss,train], feed_dict = {X : batch_xs, Y : batch_ys})
-    if step % 2000 == 0:
-        print (step, loss_val)
+# for step in xrange(0,1001):
+#     loss_val,_  = sess.run([loss,train], feed_dict = {X : batch_xs, Y : batch_ys})
+#     if step % 200 == 0:
+#         print (step, loss_val)
+# parameters
+training_epochs = 15
+batch_size = 100
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        c, _ = sess.run([loss, train], feed_dict={
+                        X: batch_xs, Y: batch_ys})
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1),
+          'loss =', '{:.9f}'.format(avg_cost))        
+
 # 评估模型
 correct_prediction = tf.equal(tf.argmax(hypothesis,1), tf.argmax(Y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 print ("Accuracy: ", accuracy.eval(session=sess, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
+# 0.88
--- python/tensorflow/mnist/neuralnetworkmnist.py
+++ python/tensorflow/mnist/neuralnetworkmnist.py
@@ -3,18 +3,16 @@
 import tensorflow as tf
 import input_data
 mnist = input_data.read_data_sets("MNIST_data/" , one_hot = True)
-batch_xs,batch_ys = mnist.train.next_batch(100)
-#  
+  
 X = tf.placeholder(tf.float32, shape= [None , 784])
 Y = tf.placeholder(tf.float32, shape= [None , 10])
-'''
-W = tf.Variable(tf.zeros([784 , 10]))
-b = tf.Variable(tf.zeros([10]))
-hypothesis = tf.nn.softmax(tf.matmul(X,W) + b)
-'''
-# weights & bias for nn layers
-# 两层
 
+W = tf.Variable(tf.random_normal([784 , 10]))
+b = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.nn.softmax(tf.matmul(X,W) + b)
+
+# weights & bias for nn layers
+'''
 W1 = tf.Variable(tf.random_normal([784, 256]))
 b1 = tf.Variable(tf.random_normal([256]))
 L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
@@ -26,12 +24,10 @@ L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
 W3 = tf.Variable(tf.random_normal([256, 10]))
 b3 = tf.Variable(tf.random_normal([10]))
 hypothesis = tf.matmul(L2, W3) + b3
+'''
 
 
-
-# 成本函数 : 利用交叉熵来描述损失函数
-#cross_entropy = -tf.reduce_sum(hypothesis * tf.log(Y))
-# define cost/loss & optimizer
+# 成本函数 : 利用交叉熵来描述损失函数 define cost/loss & optimizer
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
 train =optimizer.minimize(loss)
@@ -44,11 +40,25 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 # 拟合平面
-for step in xrange(0,10001):
-    loss_val,_  = sess.run([loss,train], feed_dict = {X : batch_xs, Y : batch_ys})
-    if step % 200 == 0:
-        print (step, loss_val)
+# parameters
+training_epochs = 15
+batch_size = 100
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        c, _ = sess.run([loss, train], feed_dict={
+                        X: batch_xs, Y: batch_ys})
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1),
+          'loss =', '{:.9f}'.format(avg_cost))        
+
 # 评估模型
 correct_prediction = tf.equal(tf.argmax(hypothesis,1), tf.argmax(Y,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print ("Accuracy: ", accuracy.eval(session=sess, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
+# softmax  AdamOptimizer 0.915 GradientDescentOptimizer 0.888 nn-laylers 0.098?什么鬼？
