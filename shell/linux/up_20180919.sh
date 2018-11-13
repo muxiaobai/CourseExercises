21c9cd6800b98df3758b5a6429e2779467724ca8
慕小白 <muxiaobai@users.noreply.github.com>
2018-11-13 19:16:37 +0800

    Update up_20180919.sh

--- shell/linux/up_20180919.sh
+++ shell/linux/up_20180919.sh
@@ -8,7 +8,7 @@
 TOMCAT_HOME="/thglxt/tomcat-8.5.20"
 WEB_APPS="$TOMCAT_HOME/web-apps"
 LOG_HOME="/thglxt/log/upgrade"
-WAR_HOME="/thglxt/bak/ROOT"
+BAK_HOME="/thglxt/bak/ROOT"
 #TOMCAT_HOME="/c/apache-tomcat-8.0.36"
 #TOMCAT_PORT=8080
 FILE_NAME=`date "+%Y%m%d%H%M%S"`
@@ -16,7 +16,7 @@ FILE_NAME=`date "+%Y%m%d%H%M%S"`
 LOG_FILE_NAME="$FILE_NAME.log"
 LOG_FILE_PATH="$LOG_HOME/$LOG_FILE_NAME"
 
-#WAR_HOME="$LOG_HOME"
+#BAK_HOME="$LOG_HOME"
 
 TIME=`date "+%Y-%m-%d %H:%M:%S"`
 #FILE_NAME="upload"
@@ -28,25 +28,25 @@ echo "LOG location:$LOG_FILE_PATH" >> "$LOG_FILE_PATH"
 echo "start Tomcat URL:$startupsh" >>"$LOG_FILE_PATH"
 echo "start tomcat URL:$shutdownsh" >>"$LOG_FILE_PATH"
 
-# first judge `WAR_HOME` has `SSH.war` if there are then next ,else shell.sh don't run .
+# first judge `BAK_HOME` has `SSH.war` if there are then next ,else shell.sh don't run .
 
 # if has SSH.war then rename ROOT.war
 
 #echo "rename Forp-DTH-Product.zip==>ROOT.zip" >> "$LOG_FILE_PATH"
-#cp $WAR_HOME/Forp-DTH-Product.zip $WAR_HOME/ROOT.zip
+#cp $BAK_HOME/Forp-DTH-Product.zip $BAK_HOME/ROOT.zip
 # if tomcat is run should stop .else skip this  `stop tomcat` step 
 
 #copy old war 
 echo "Old War Name $FILE_NAME.war" >>"$LOG_FILE_PATH"
-cp $WEB_APPS/ROOT.zip $WAR_HOME/$FILE_NAME.zip
+cp $WEB_APPS/ROOT.zip $BAK_HOME/$FILE_NAME.zip
 
 # remove old war and file
 #rm -rf $TOMCAT_HOME/webapps/ROOT
 #sleep 2s;
-
+tar -czvf xxxx.tar.gz -C $WEB_APPS/ ROOT/
 #copy new war
 echo " $TIME copy new war..." >>"$LOG_FILE_PATH"
-cp $WAR_HOME/ROOT.zip $WEB_APPS/ROOT.zip
+cp $BAK_HOME/ROOT.zip $WEB_APPS/ROOT.zip
 
 # stop tomcat
 echo "$TIME stoping tomcat..." >>"$LOG_FILE_PATH"
