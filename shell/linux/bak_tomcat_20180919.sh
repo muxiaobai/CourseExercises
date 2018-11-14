eea91a2170f92b6784e0505ebf17effb6e95f9ce
慕小白 <muxiaobai@users.noreply.github.com>
2018-11-14 11:45:29 +0800

    Update bak_tomcat_20180919.sh

--- shell/linux/bak_tomcat_20180919.sh
+++ shell/linux/bak_tomcat_20180919.sh
@@ -5,22 +5,32 @@
 #chmod +x ./test.sh  
 #./test.sh  
 #set fileformat = unix
+UP_BAK_HOME="/thglxt/bak"
+BAK_HOME="$UP_BAK_HOME/bak"
+UP_HOME="$UP_BAK_HOME/upgrade"
 TOMCAT_HOME="/thglxt/tomcat-8.5.20"
 WEB_APPS="$TOMCAT_HOME/web-apps"
-LOG_HOME="/data/log/bak"
-WAR_HOME="/thglxt/bak/ROOT"
-#TOMCAT_HOME="/c/apache-tomcat-8.0.36"
-TOMCAT_PORT=8080
+BAK_LOG_PATH="$BAK_HOME/log"
+BAK_ROOT="$BAK_HOME/ROOT"
+
+# mkdir path
+if [ ! -d "$BAK_ROOT" ];then
+mkdir $BAK_ROOT
+else
+echo "$BAK_ROOT 文件夹已经存在"
+fi
+
+if [ ! -d "$BAK_LOG" ];then
+mkdir $BAK_LOG
+else
+echo " $BAK_LOG 文件夹已经存在"
+fi
+
+currentTime=`date "+%Y-%m-%d %H:%M:%S"`
 FILE_NAME=`date "+%Y%m%d%H%M%S"`
-#LOG_HOME="/c/Users/zhang/Desktop"
 LOG_FILE_NAME="$FILE_NAME.log"
 LOG_FILE_PATH="$LOG_HOME/$LOG_FILE_NAME"
 
-#WAR_HOME="$LOG_HOME"
-
-TIME=`date "+%Y-%m-%d %H:%M:%S"`
-#FILE_NAME="upload"
-currentTime=`date "+%Y-%m-%d %H:%M:%S"`
 echo "begin time :$currentTime" >> "$LOG_FILE_PATH"
 startupsh="$TOMCAT_HOME/bin/startup.sh"
 shutdownsh="$TOMCAT_HOME/bin/shutdown.sh"
@@ -28,25 +38,24 @@ echo "LOG location:$LOG_FILE_PATH" >> "$LOG_FILE_PATH"
 echo "start Tomcat URL:$startupsh" >>"$LOG_FILE_PATH"
 echo "stop Tomcat URL:$shutdownsh" >>"$LOG_FILE_PATH"
 
+# tar running ROOT
 
-#copy last war 
-cd $WAR_HOME
+#copy last tar.gz 
 newest_file_of()
 {
-        ls -t "$@" | head -1
+        ls $BAK_HOME -t "$@" | head -1
 }
 
 echo "newest file of *.war is $(newest_file_of *.war)"
 LAST_NAME=$(newest_file_of *.war)
 
 echo "last file: $LAST_NAME" >> "$LOG_FILE_PATH"
-cp $WAR_HOME/$LAST_NAME $TOMCAT_HOME/webapps/ROOT.war
 
 
 
 # if tomcat is run should stop .else skip this  `stop tomcat` step 
 
-echo "$TIME stop Tomcat..." >>"$LOG_FILE_PATH"
+echo "$currentTime stop Tomcat..." >>"$LOG_FILE_PATH"
 $shutdownsh
 echo "tomcat stoped" >>"$LOG_FILE_PATH"
 sleep 2s;
@@ -63,7 +72,7 @@ unzip -o ROOT.zip
 
 #start tomcat 
 cd $TOMCAT_HOME
-echo " $TIME start Tomcat..." >>"$LOG_FILE_PATH"
+echo " $currentTime start Tomcat..." >>"$LOG_FILE_PATH"
 $startupsh
-echo "$TIME Tomcat started..." >>"$LOG_FILE_PATH"
+echo "$currentTime Tomcat started..." >>"$LOG_FILE_PATH"
 
