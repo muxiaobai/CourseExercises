5c40482fa5e60c35a8905b417fe3ba9eb3606233
慕小白 <muxiaobai@users.noreply.github.com>
2018-11-27 11:28:24 +0800

    Update up_roll_2018.sh

--- shell/up_roll_2018.sh
+++ shell/up_roll_2018.sh
@@ -24,7 +24,8 @@ TOMCAT_HOME="/thglxt/tomcat-8.5.20"
 
 startupsh="$TOMCT_HOME/bin/startup.sh"
 shutdownsh="$TOMCAT_HOME/bin/shutdown.sh"
-WEB_APPS="$TOMCAT_HOME/web-apps"
+WEB_APPS="$TOMCAT_HOME/web-apps/"
+WEB_ROOT="$TOMCAT_HOME/web-apps/ROOT"
 
 FILE_NAME=`date "+%Y%m%d%H%M%S"`
 currentTime=`date "+%Y-%m-%d %H:%M:%S"`
@@ -40,41 +41,42 @@ BAK_ROOT="$BAK_HOME/ROOT"
 BAK_LOG="$BAK_HOME/log"
 BAK_LOG_FILE="$BAK_LOG/roll_$FILE_NAME.log"
 
+currentTime=`date "+%Y-%m-%d %H:%M:%S"`
 
 #==========================================upgrade============================================================
 # upgrade
 if [ $PARAM = 'upgrade' ]; then
+echo  '=================upgrade: $currentTime=========================' 
 
-echo  'upgrade'  >> "$UP_LOG_FILE"
 #===================dir ==============================
 
 #文件夹 
 if [ ! -d $UP_HOME ];then
 mkdir $UP_HOME
 else
-echo "exit $UP_HOME"  >> "$UP_LOG_FILE"
+echo "exit $UP_HOME" 
 fi
 
 if [ ! -d $UP_LOG ];then
 mkdir $UP_LOG
 else
-echo "exit $UP_LOG"  >> "$UP_LOG_FILE"
+echo "exit $UP_LOG" 
 fi
 
 if [ ! -d $UP_ROOT ];then
 mkdir $UP_ROOT
 else
-echo "exit $UP_ROOT"  >> "$UP_LOG_FILE"
+echo "exit $UP_ROOT" 
 fi
 
+touch "$UP_LOAD_FILE"
+echo "begin time:$currentTime" >> "$UP_LOG_FILE"
 
 #=================================================
 #copy old war 
 echo "Old War Name $UP_HOME/$FILE_NAME.tar.gz" >>"$UP_LOG_FILE"
 tar -czvf  $UP_ROOT/$FILE_NAME.tar.gz -C $WEB_APPS/ ROOT/
 #cp $WEB_APPS/ $BAK_HOME/$FILE_NAME.zip
-
-
 
 # stop tomcat
 echo "$currentTime stoping tomcat..." >>"$UP_LOG_FILE"
@@ -90,6 +92,7 @@ unzip -o $UP_HOME/ROOT.zip -d $WEB_APPS
 sleep 2s;
 
 # ps -ef | grep tomcat 没有的时候可以下面执行
+
 # remove cache
 echo " rm -rf $TOMCAT_HOME/work " >> "$UP_LOG_FILE"
 rm -rf $TOMCAT_HOME/work/Catalina
@@ -101,7 +104,7 @@ echo "$currentTime Tomcat started..." >>"$UP_LOG_FILE"
 
 
 # 执行了这些之后，直接退出，因为upgrade 和rollback是只能选择其中一个
-exit 1;
+exit 0;
 fi
 
 #==========================================rollback=================================================================
@@ -109,59 +112,90 @@ fi
 # rollback
 if  [ $PARAM = 'rollback' ];then 
 
-echo 'rollback'  >> "$BAK_LOG_FILE"
+echo ' ===================rollback: $currentTime=========================' 
 
-#=================================================
 
+#=====================dir============================
 #文件夹 
 if [ ! -d $BAK_HOME ];then
 mkdir $BAK_HOME
 else
-echo "exit $BAK_HOME"  >> "$BAK_LOG_FILE"
+echo "exit $BAK_HOME"  
 fi
 
 if [ ! -d $BAK_LOG ];then
 mkdir $BAK_LOG
 else
-echo "exit $BAK_LOG"  >> "$BAK_LOG_FILE"
+echo "exit $BAK_LOG"  
 fi 
 
 if [ ! -d $BAK_ROOT ];then
 mkdir $BAK_ROOT
 else
-echo "exit $BAK_ROOT" >>$BAK_LOG_FILE
+echo "exit $BAK_ROOT" 
+fi
+
+
+if [ ! -d $UP_ROOT ];then
+
+echo "no upgrade, and can't rollback, not exit $UP_ROOT"
+exit 1
+
+else
+echo "exit $UP_ROOT" 
 fi
 
 #=================================================
 
 # rollbakc Log_File
 
-BAK_LOG_FILE=$BAK_LOG/$FILE_NAME.log
+touch "$BAK_LOG_FILE"
+echo "begin  rollback time:$currentTime" >> "$BAK_LOG_FILE"
 
-currentTime=`date "+%Y-%m-%d %H:%M:%S"`
-echo $BAK_LOG_FILE
-touch $BAK_LOG_FILE
 
 #tar -czvf "$BAK_ROOT/$FILE_NAME.tar.gz" -C $WEB_APPS ROOT/
+# last new tar.gz in $UP_ROOT
 
 newest_file_of(){
 ls $UP_ROOT -t "$@" | head -1
 }
-echo "$(newest_file_of)  last new tar.gz ">> $BAK_LOG_FILE
+
 OLD_FILE=$(newest_file_of)
+echo "$OLD_FILE last new tar.gz ">> $BAK_LOG_FILE
 
 # 没有upgrade过的没有rollback old_files
 
-#=================================================
-
-
-
-
-
-
 #============stop  start==========================
 
-exit 1;
+# stop tomcat
+echo "$currentTime stoping tomcat..." >>"$BAK_LOG_FILE"
+$shutdownsh >> "$UP_LOG_FILE"
+echo "$currentTime tomcat stoped" >>"$BAK_LOG_FILE"
+sleep 2s;
+
+# 移除原来的ROOT运行环境
+
+rm -rf $WEB_APPS/ROOT/
+sleep 2s;
+
+# tar -xzvf 
+echo  "tar -xzvf $UP_ROOT/$OLD_FILE $WEB_APPS">> "$BAK_LOG_FILE"
+tar -xzvf $UP_ROOT/$OLD_FILE $WEB_APPS
+
+# ps -ef | grep tomcat 没有的时候可以下面执行
+
+# remove cache
+echo " rm -rf $TOMCAT_HOME/work " >> "$BAK_LOG_FILE"
+rm -rf $TOMCAT_HOME/work/Catalina
+
+#start tomcat 
+echo " $currentTime start Tomcat..." >>"$BAK_LOG_FILE"
+$startupsh >> "$BAK_LOG_FILE"
+echo "$currentTime Tomcat started..." >>"$BAK_LOG_FILE"
+
+#============stop rollback start==========================
+
+exit 0;
 
 fi
 
